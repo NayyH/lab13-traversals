2153ac2e447f41ada34bce3f346a07485ef639e8
Jay <machaojie0611@gmail.com>
2023-04-28 12:46:50 -0700

    in order

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -165,7 +165,12 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+		if (node.data == null) {
+			return;
+		}
+		inOrderRecurse(node.leftChild);
+		System.out.print(node.data + " ");
+		inOrderRecurse(node.rightChild);
 	}
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
